ef768c531e037a8c9faeb2c36f268c98980bbfe7
batman06 <batman06@2531c1ff-93ec-4438-9692-90ac7cfe5fc8>
2020-07-26 18:15:45 +0000

    requestResponder: build fix; release added

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 mvn clean compile package
+
+if ! [ -d $JavaDevelop_PATH/Projects/requestResponder/requestResponder ]; then
+mkdir $JavaDevelop_PATH/Projects/requestResponder/requestResponder
+fi
+if ! [ -d $JavaDevelop_PATH/Projects/requestResponder/requestResponder/lib ]; then
+mkdir $JavaDevelop_PATH/Projects/requestResponder/requestResponder/lib
+fi
+
 echo copy executeable jar to output project:
 cp $JavaDevelop_PATH/Projects/requestResponder/target/*.jar $JavaDevelop_PATH/Projects/requestResponder/requestResponder/
 echo copy libs to output project:
